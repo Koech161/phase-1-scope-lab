8c7a06933e00508a278663905b4872c87869b51e
Abel Koech <koechabel161@gmail.com>
2024-06-27 13:27:17 +0300

    scope

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Write your solution in this file!
+ window.customerName = 'bob'
+ const leastFavoriteCustomer= 'Ben'
+ function upperCaseCustomerName(){ 
+    if(window.customerName){
+        window.customerName=window.customerName.toUpperCase();
+    }
+ }
+ function setBestCustomer(){
+    window.bestCustomer='not bob'
+ } 
+  function overwriteBestCustomer(){
+window.bestCustomer='maybe bob'
+ } 
+
+ function changeLeastFavoriteCustomer(favorite){
+   leastFavoriteCustomer= favorite
+  
+ }
